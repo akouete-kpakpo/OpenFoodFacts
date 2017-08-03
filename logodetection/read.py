b7ccad8bb0a03bf72a6295144652a2fe8fafbb05
KevinKpakpo <adkevinkpakpo@gmail.com>
2017-08-03 19:26:46 +0200

    Using subimages

--- logodetection/read.py
+++ logodetection/read.py
@@ -15,7 +15,7 @@ from keras.preprocessing.image import (img_to_array,
                                        #load_img
                                        )
 
-def read_from_disk(path, npixel = (300, 400), nimages = 1044, nbatches = 10):
+def read_from_disk(path, npixel = (96, 96), nimages = 1044, nbatches = 10):
     """
         Reads the images data stored on the disk
         
@@ -42,38 +42,46 @@ def read_from_disk(path, npixel = (300, 400), nimages = 1044, nbatches = 10):
     def _add_img_to_array(imgs,img_name):
         print("Ajout de l'image {} au dataset".format(img_name))
         img = Image.open(os.path.join(path,img_name))
-        img = img.resize((npixel[0],npixel[1]))
-        x = img_to_array(img)
-        x = x.astype(np.uint8)
-        #x = x.reshape(1,-1)
-        x = np.expand_dims(x, axis=0)
-        if x.shape != (1, npixel[1],npixel[0],3):
+        img = img.resize((npixel[0]*3,npixel[1]*4))
+        
+        if img_to_array(img).shape != (npixel[1]*4,npixel[0]*3,3):
             #The image is black
-            x = np.zeros((1, npixel[1],npixel[0],3))
-            print("The image {} is black.".format(f))
-        imgs = np.vstack([imgs, x])
-##        x = np.array([f,x])
-#        if x.shape != (1, npixel[1]*npixel[0]*3 + 1):
-#            #The image is black
-#            x = np.zeros((1, npixel[1]*npixel[0]*3 + 1))
-#            print("The image {} is black.".format(f))
-#        imgs = np.vstack([imgs, x])
+            print("The image {} is black.".format(img_name))
+            imgs = np.vstack([imgs, np.zeros((12,npixel[1],npixel[0],3))])
+        else:
+            width = npixel[0]
+            height = npixel[1]
+            #imgs = np.empty(shape = (0,npixel[1],npixel[0],3), dtype = np.uint8)
+            for nleft in range(3):
+                left = nleft*96
+                for ntop in range(4):
+                    top = ntop*96
+                    box = (left, top, left+width, top+height)
+                    subimg = img.crop(box)
+                    #print("Ajout de la portion  {} de cette image au dataset".format((nleft, ntop)))
+                    x = img_to_array(subimg)
+                    x = x.astype(np.uint8)
+                    #x = x.reshape(1,-1)
+                    x = np.expand_dims(x, axis=0)
+                    imgs = np.vstack([imgs, x])
+        
         return imgs
     
-    
-    n = 2*nimages//nbatches
-    data = np.empty(shape = (0,n,npixel[1],npixel[0],3))
-    labels = np.empty(shape = (0,n))
+    nsubimg = 3*4
+    n_images_per_batch = 2*nimages//nbatches
+    n_sub_images_per_batch = 2*nimages//nbatches*nsubimg
+    data = np.empty(shape = (0,n_sub_images_per_batch,npixel[1],npixel[0],3))
+    labels = np.empty(shape = (0,n_sub_images_per_batch))
     for batch in range(nbatches):
-        s = np.arange(n*batch,n*(batch+1))
+        s = np.arange(n_images_per_batch*batch,n_images_per_batch*(batch+1))
         imgs = np.empty(shape = (0, npixel[1],npixel[0],3), dtype = np.uint8)
-        batch_label = np.array([])
+        batch_label = np.array([], dtype = np.bool_)
         for f in images[s]:
             if f.startswith('a'):
                 #Auchan coded with 0
-                batch_label = np.hstack([batch_label, '0'])
+                batch_label = np.hstack([batch_label, np.zeros(nsubimg, dtype = np.bool_)])
             else:
-                batch_label = np.hstack([batch_label, '1'])
+                batch_label = np.hstack([batch_label, np.ones(nsubimg, dtype = np.bool_)])
             imgs = _add_img_to_array(imgs,f)
         imgs = np.expand_dims(imgs, axis=0)
         batch_label = np.expand_dims(batch_label, axis=0)
@@ -102,7 +110,7 @@ if __name__ == '__main__':
     np_path = '/home/kevin/Desktop/OpenFoodFacts/data/npy'
     imgs_np_path = os.path.join(np_path, 'images.npy')
     labels_np_path = os.path.join(np_path, 'labels.npy')
-    npixel = (300,400)
+    npixel = (96,96)
     (data, labels) = read_from_disk(images_path)
     #TODO: cette opération prend du temps
     #Afficher une jauge de progression plutôt que le nom des images ajoutées
